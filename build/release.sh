a0cbf7254375fa84bd7d138dccb46f52087975f8
Tyll Weiß <tyll.weiss.extern@axelspringer.com>
2017-12-04 12:15:52 +0100

    TASK: Add further debug logs in regards to #14

--- build/release.sh
+++ build/release.sh
@@ -8,7 +8,8 @@ set -e
 # Do not run this script on your local installation since
 # releases will be done via CI.
 #
-ls -lah packages/create-react-microservice-scaffold/src/packages/create-react-microservice-test-scaffold-components/
+echo "1"
+ls -lah packages/create-react-microservice-scaffold/src/packages/my-fancy-ui-components/
 npx semantic-release pre
 
 # Read the generated version from the package.json.
@@ -20,16 +21,23 @@ VERSION="$(echo $VERSION | sed -e 's/^"//' -e 's/"$//')"
 # Create the required .npmrc file which points the `npm` CLI to use the `NPM_TOKEN``for auth.
 echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" > ~/.npmrc
 
-ls -lah packages/create-react-microservice-scaffold/src/packages/create-react-microservice-test-scaffold-components/
+echo "2"
+ls -lah packages/create-react-microservice-scaffold/src/packages/my-fancy-ui-components/
 
 # Perform a hard reset to avoid changed/missing files.
 git reset --hard
 
-ls -lah packages/create-react-microservice-scaffold/src/packages/create-react-microservice-test-scaffold-components/
+echo "3"
+ls -lah packages/create-react-microservice-scaffold/src/packages/my-fancy-ui-components/
 
 echo "Releasing $VERSION ..."
-ls -lah packages/create-react-microservice-scaffold/src/packages/create-react-microservice-test-scaffold-components/
+echo "4"
+ls -lah packages/create-react-microservice-scaffold/src/packages/my-fancy-ui-components/
 npx lerna publish --skip-git --yes --exact --repo-version=$VERSION
-ls -lah packages/create-react-microservice-scaffold/src/packages/create-react-microservice-test-scaffold-components/
+
+echo "5"
+ls -lah packages/create-react-microservice-scaffold/src/packages/my-fancy-ui-components/
 npx semantic-release post
-ls -lah packages/create-react-microservice-scaffold/src/packages/create-react-microservice-test-scaffold-components/
+
+echo "6"
+ls -lah packages/create-react-microservice-scaffold/src/packages/my-fancy-ui-components/
